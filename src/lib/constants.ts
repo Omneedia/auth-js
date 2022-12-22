991ee802722ed09d71450a10195535d0108448e5
Stéphane ZUCATTI <stephane.zucatti@me.com>
2022-12-22 19:54:38 +0100

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,6 @@ export const AUDIENCE = ''
 export const DEFAULT_HEADERS = { 'X-Client-Info': `gotrue-js/${version}` }
 export const EXPIRY_MARGIN = 10 // in seconds
 export const NETWORK_FAILURE = {
-  MAX_RETRIES: 10,
+  MAX_RETRIES: 11,
   RETRY_INTERVAL: 2, // in deciseconds
 }
